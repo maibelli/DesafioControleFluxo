bce8546629d884598af50b967a26301647ea801d
ma ibelli <mayara_ibelli@hotmail.com>
2024-07-01 20:41:53 -0300

    first commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+// Arquivo: ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
